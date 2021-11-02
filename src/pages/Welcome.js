9c5e4741f5830d43b69dd73e3f918d41462d1c8c
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2021-11-02 23:36:19 +0300

    Evaluates nested routes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,7 +1,12 @@
+import { Route } from "react-router-dom";
+
 const Welcome = () => {
     return (
         <div>
             <h2>The Welcome Page</h2>
+            <Route path="/welcome/new-user">
+                <p>Welcome, new user</p>
+            </Route>
         </div>
 
     );
